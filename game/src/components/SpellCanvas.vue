5cb4e0b01b5dfb3e3c366e8811115e13371d80d4
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Add component for spell canvas SpellCanvas.vue

---
+++ game/src/components/SpellCanvas.vue
@@ -0,0 +1,30 @@
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<script>
+export default {
+  props: ['runAnimationTrigger', 'animation', 'verticalAxis'],
+  computed: {
+    ctx: function() {
+      return this.$refs.canvas.getContext('2d');
+    },
+    height: function() {
+      this.ctx.canvas.height;
+    },
+    width: function() {
+      this.ctx.canvas.width;
+    }
+  },
+  methods: {
+    playAnimation() {
+      this.animation(this.ctx, this.verticalAxis);
+    }
+  },
+  watch: {
+    runAnimationTrigger: function() {
+      this.playAnimation();
+    }
+  }
+};
+</script>
