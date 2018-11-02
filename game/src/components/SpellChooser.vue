1335ab2b16d55ead5f66a50fc5842b011dc280bb
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Add SpellChooser.vue

---
+++ game/src/components/SpellChooser.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <b-radio-group id="spells"
+      buttons
+      stacked
+      v-model="selected"
+      button-variant="outline-secondary"
+      size="lg"
+      :options="options"
+      name="radioBtnStacked" 
+      class="btn-block"/>
+  </div> 
+</template>
+
+<script>
+export default {
+  props: ['spells'],
+  data() {
+    return {
+      selected: undefined,
+      centered: false
+    };
+  },
+  computed: {
+    options() {
+      return this.spells.map(spell => {
+        return { value: spell.name, text: `${spell.symbol} ${spell.name}` };
+      });
+    }
+  },
+  watch: {
+    selected: function(value) {
+      this.$parent.$emit('spellSet', value);
+      console.log('SPELL CHOOSER: selected', value);
+    }
+  }
+};
+</script>
+
+<style scoped>
+strong {
+  color: unset;
+}
+</style>
