755b3a2c6e87d5e7d0e688727ffa5e371df80155
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Add data for model

---
+++ game/src/assets/data/model/model.js
@@ -0,0 +1,8 @@
+import sheet_face from './sheet_face.png';
+import sheet_hair from './sheet_hair.png';
+import sheet_pants from './sheet_pants.png';
+import sheet_shirts from './sheet_shirts.png';
+import sheet_shoes from './sheet_shoes.png';
+import sheet_skin from './sheet_skin.png';
+
+export default [sheet_face, sheet_hair, sheet_pants, sheet_shirts, sheet_shoes, sheet_skin];
