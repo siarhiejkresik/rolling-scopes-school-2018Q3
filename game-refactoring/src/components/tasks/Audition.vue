0a43fca16ded5ec01fe57a21d9384ee03f0a36b9
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    tasks: some minor chages in the audition task component

--- game-refactoring/src/components/tasks/Audition.vue
+++ game-refactoring/src/components/tasks/Audition.vue
@@ -4,19 +4,23 @@
       <b-button
         size="lg"
         @click="sayWord"
-        class="play mx-auto mt-5 mb-5">{{ played ? 'Паўтарыць' : 'Праслухаць' }}</b-button>
-      <b-form-input
-        v-model="answer"
-        type="text"
-        size="lg"
-        placeholder="прыклад адказу: мама"
-        class="mt-3"></b-form-input>
+        class="play mx-auto mt-5 mb-5">{{ isFirstPlay ? 'Праслухаць' :'Паўтарыць' }}</b-button>
+      <b-form @submit="sendAnswerReady">
+        <b-form-input
+          v-model="answer"
+          type="text"
+          size="lg"
+          placeholder="прыклад адказу: мама"
+          class="mt-3">
+        </b-form-input>
+      </b-form>
     </div>  
 </template>
 
 <script>
 import Mixin from './Mixin.js';
 import { randomArrayElement } from '../../scripts/utils.js';
+
 import translations from '../../assets/data/translations.json';
 
 export default {
@@ -24,15 +28,14 @@ export default {
   data() {
     return {
       lang: 'en-GB',
-      translations: translations,
+      isFirstPlay: true,
       answer: null,
-      word: undefined,
-      played: false
+      word: undefined
     };
   },
   computed: {
     correctAnswers() {
-      return this.translations[this.word];
+      return translations[this.word];
     },
     isCorrectAnswer() {
       return this.word !== undefined && this.correctAnswers.includes(this.answer);
@@ -40,12 +43,10 @@ export default {
   },
   methods: {
     generateNewTask() {
-      this.played = false;
-      this.answer = null;
-      this.word = randomArrayElement(Object.keys(this.translations));
+      this.word = randomArrayElement(Object.keys(translations));
     },
     sayWord() {
-      this.played = true;
+      this.isFirstPlay = false;
       const utterance = new SpeechSynthesisUtterance(this.word);
       utterance.lang = this.lang;
       window.speechSynthesis.speak(utterance);
@@ -59,5 +60,3 @@ export default {
   display: block;
 }
 </style>
-
-
