89bf15960a32245a7df5fe872516bca8914bd81b
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Add a spinner to LoadingView.vue and a possibility to set a loading message

--- game-refactoring/src/screens/LoadingView.vue
+++ game-refactoring/src/screens/LoadingView.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="display-4">ПАЧАКАЙЦЕ...</div>
+  <div class="container flex-centered">
+    <div class="content flex-centered flex-column">
+      <div
+        v-if="message"
+        class="display-4">{{ message }}</div>
+      <div class="spinner m-4"></div>
+    </div>
+  </div>
 </template>
+
 <script>
 export default {
+  props: ['message'],
   computed: {
     isLoaded() {
       return this.$store.state.assets.isLoaded;
@@ -17,3 +26,36 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.container {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.flex-centered {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.spinner {
+  border-top: 3px solid var(--primary);
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
