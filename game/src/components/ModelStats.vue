d708059e7a0bfbac25e57ad20ab4266d23337776
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Add ModelStats.vue component

---
+++ game/src/components/ModelStats.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <div class="name">ІМЯ: {{ name }}</div>
+      <div class="health">ЖЫЦЦЁ: {{ health }}</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  props: ['name', 'health']
+};
+</script>
+
+<style scoped>
+div {
+  color: white;
+  font-size: 20px;
+}
+</style>
+
+
