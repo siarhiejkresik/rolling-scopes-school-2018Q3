7fda4ccf657ca672c2b17322dd1f1e609f4b91bb
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Updage config for storebook

--- game-refactoring/.storybook/config.js
+++ game-refactoring/.storybook/config.js
@@ -8,11 +8,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import LoginView from '../src/screens/LoginView.vue';
+import Modal from '../src/components/Modal.vue';
+import ModalWrapper from '../src/stories/ModalWrapper.vue';
 
 Vue.use(Vuex);
 Vue.use(BootstrapVue);
 
 Vue.component('login-view', LoginView);
+Vue.component('modal', Modal);
+Vue.component('modal-wrapper', ModalWrapper);
 
 function loadStories() {
   require('../src/stories/index.js');
