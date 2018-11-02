824fab880041285a08a8c8fd2374c58b37569483
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Add LoginView.vue

---
+++ game/src/components/LoginView.vue
@@ -0,0 +1,46 @@
+<template>
+  <b-form
+      @submit="onSubmit"
+      class="login-form">
+    <b-form-group
+        class="display-4"
+        label="Увядзіце сваё імя"
+        label-for="playerNameInput">
+      <b-form-input
+          id="playerNameInput"
+          v-model="playerName"
+          type="text"
+          size="lg"
+          placeholder=""
+          required>
+      </b-form-input>
+      <b-button
+        type="submit"
+        size="lg"
+        variant="primary"
+        class="d-block mx-auto mt-4">Гуляць</b-button>
+    </b-form-group>
+  </b-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      playerName: null
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$emit('logIn', this.playerName);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-form {
+  min-width: 500px;
+}
+</style>
