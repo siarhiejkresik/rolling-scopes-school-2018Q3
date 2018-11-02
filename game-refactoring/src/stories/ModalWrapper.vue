6e7b61f71a30931c73c098e71f4176512054c1b1
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Add a component for showing Modal.vue functionality in storybook

---
+++ game-refactoring/src/stories/ModalWrapper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <modal
+      v-model="show"
+      hascancel=true
+      title='LOREM IPSUM'
+      @ok="onOk"
+      @cancel="onCancel">
+        {{ lorem }}
+    </modal> 
+    
+    <div class="d-flex flex-column m-auto align-items-center">
+      <b-button
+        class="m-3"
+        @click="showModal">Show</b-button>
+      <div v-if="!show" class="m-1">{{ event }}</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { LOREM } from './constants.js';
+
+export default {
+  data: function() {
+    return {
+      show: false,
+      event: '',
+      lorem: LOREM
+    };
+  },
+  methods: {
+    showModal() {
+      this.show = !this.show;
+      this.event = '';
+    },
+    onOk() {
+      this.event = 'ok pressed';
+    },
+    onCancel() {
+      this.event = 'cancel pressed';
+    }
+  }
+};
+</script>
---
+++ game-refactoring/src/stories/constants.js
@@ -0,0 +1,5 @@
+const LOREM = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iusto facilis, magnam magni iste quidem aliquid voluptates minus fugit, nulla blanditiis quaerat atque quo. Quaerat rerum nam tempora eaque quod?
+Ducimus iusto excepturi itaque magni soluta molestias facere velit. Repudiandae officia dolorem beatae ducimus eveniet, quaerat molestiae veniam harum earum quidem natus amet exercitationem quam laboriosam maiores tempore, consequatur fugiat!
+Architecto corrupti fugit officiis non consequuntur sapiente ipsam, dicta cum consequatur quo itaque aperiam quidem, reiciendis, eum deserunt odio magni doloremque. Obcaecati quos ipsum esse eius aperiam? Hic, fugiat ad.`;
+
+export { LOREM };
