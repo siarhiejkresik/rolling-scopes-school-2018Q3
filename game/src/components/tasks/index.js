cc101d0977660f49b7ba61a9e2a3e566faa75654
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Add a task components aggregator file

---
+++ game/src/components/tasks/index.js
@@ -0,0 +1,6 @@
+import Arithmethic from './Arithmethic.vue';  
+import Audition from './Audition.vue';    
+import Sorting from './Sorting.vue';  
+import Translation from './Translation.vue';  
+
+export { Arithmethic, Audition, Sorting, Translation };
