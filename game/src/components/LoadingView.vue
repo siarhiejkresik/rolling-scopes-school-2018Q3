394e739c8b6faf743f98dcd9e4b5b69eb25e5d7c
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Add LoadingView.vue

---
+++ game/src/components/LoadingView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="display-4">ПАЧАКАЙЦЕ...</div>
+</template>
+<script>
+export default {
+  computed: {
+    isLoaded() {
+      return this.$store.state.assets.isLoaded;
+    }
+  },
+  watch: {
+    isLoaded: function() {
+      if (this.isLoaded) {
+        this.$emit('loaded');
+      }
+    }
+  }
+};
+</script>
