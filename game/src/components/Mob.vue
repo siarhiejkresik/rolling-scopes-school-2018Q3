b3637ae641c862b4a2b5bd75b620a2d6145309b6
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Refactor and cleanup Mob.vue

--- game/src/components/Mob.vue
+++ game/src/components/Mob.vue
@@ -3,85 +3,78 @@
 
 <script>
 import { randomObjectElement } from '../scripts/utils.js';
-import getRandomName from '../scripts/Monster.js';
-import parts from '../assets/images/models/parts.json';
-import { MonsterModel } from '../scripts/MonsterModel.js';
 
 export default {
   props: {
     verticalAxis: Number,
-    bottomLine: Number
+    bottomLine: Number,
+    model: Object,
+    scale: Number,
+    renderTrigger: Boolean
   },
   computed: {
-    name() {
-      return getRandomName();
-    },
     ctx() {
       return this.$parent.ctx; // TODO: bad
     }
   },
-  mounted: function() {
-    // console.log('mounted mob', this.ctx);
-    // let model = new MonsterModel();
-    // this.model.loadAtlases();
-    // this.model.createRandomMonster();
-
-    const skin = randomObjectElement(parts.skin);
-    const shirtsArm = randomObjectElement(parts.shirts.arm);
-    const shoes = randomObjectElement(parts.shoes);
-    const shirt = randomObjectElement(parts.shirts.shirt);
-    const hair = randomObjectElement(parts.hair.man);
-    const pantsLeg = randomObjectElement(parts.pants.leg);
-    const pants = randomObjectElement(parts.pants.pants);
-
-    const shirtsArmOffsets = {
-      x: { long: 125, shorter: 85, short: 105 },
-      y: 217
-    };
-    const skinHandOffsets = { x: 200, y: 320 };
-    const skinArmOffsets = { x: 120, y: 216 };
-    const skinLegOffsets = { x: 52, y: 410 };
-
-    const pantsLegOffsets = { x: 52, y: 400 };
-    const pantsOffsets = { x: 0, y: 384 };
-
-    const shoesOffsets = { x: 84, y: 555 };
-    const shirtOffsets = { x: 0, y: 217 };
-
-    const headOffsets = { x: 0, y: 50 };
-    const hairOffsets = { x: 0, y: 30 };
-    const neckOffsets = { x: 0, y: 210 };
-
-    this.drawElement(skin.arm, skinArmOffsets);
-    this.drawElement(skin.arm, skinArmOffsets, true);
-    this.drawElement(skin.leg, skinLegOffsets);
-    this.drawElement(skin.leg, skinLegOffsets, true);
-    this.drawElement(skin.hand, skinHandOffsets);
-    this.drawElement(skin.hand, skinHandOffsets, true);
-    this.drawElement(
-      shirtsArm,
-      { x: shirtsArmOffsets.x[shirtsArm.length], y: shirtsArmOffsets.y },
-      true
-    );
-    this.drawElement(shirtsArm, { x: shirtsArmOffsets.x[shirtsArm.length], y: shirtsArmOffsets.y });
-    // this.drawNeck(skin.neck);
-    this.drawElement(skin.neck, neckOffsets);
-    // this.drawHead(skin.head);
-    this.drawElement(skin.head, headOffsets);
-    this.drawElement(parts.face.face.face1, { x: 0, y: 95 });
-    // this.drawHair(hair);
-    this.drawElement(hair, hairOffsets);
-    // this.drawShirt(shirt);
-    this.drawElement(shirt, shirtOffsets);
-    this.drawElement(pantsLeg, pantsLegOffsets);
-    this.drawElement(pantsLeg, pantsLegOffsets, true);
-    this.drawElement(shoes, shoesOffsets);
-    this.drawElement(shoes, shoesOffsets, true);
-    this.drawElement(pants, pantsOffsets);
-
-  },
   methods: {
-    drawElement(element, offsets, mirror = false, scale = 0.6) {
+    draw() {
+      // offsets
+      const shoesOffsets = { x: 84, y: -this.model.shoes.height };
+
+      const skinLegOffsets = {
+        x: 52,
+        y: -this.model.shoes.height - this.model.skin.leg.height + 15
+      };
+      const pantsOffsets = { x: 0, y: skinLegOffsets.y - this.model.pants.height + 20 };
+      const pantsLegOffsets = { x: 52, y: pantsOffsets.y + 20 };
+      const shirtOffsets = { x: 0, y: pantsOffsets.y - this.model.shirt.height + 10 };
+      const shirtsArmOffsets = {
+        x: { long: 125, shorter: 80, short: 105 },
+        y: shirtOffsets.y
+      };
+      const skinArmOffsets = { x: 120, y: shirtOffsets.y };
+      const skinHandOffsets = { x: 200, y: skinArmOffsets.y + 100 };
+
+      const neckOffsets = { x: 0, y: shirtOffsets.y - this.model.skin.neck.height + 30 };
+      const headOffsets = { x: 0, y: neckOffsets.y - this.model.skin.head.height + 10 };
+      const hairOffsets = {
+        x: 0,
+        y: headOffsets.y + this.model.skin.head.height / 2 - this.model.hair.height + 15
+      };
+      const faceOffsets = {
+        x: 0,
+        y: headOffsets.y + this.model.skin.head.height / 2 - this.model.face.height / 2 + 10
+      };
+
+      // draw parts
+      this.drawElement(this.model.skin.arm, skinArmOffsets);
+      this.drawElement(this.model.skin.arm, skinArmOffsets, true);
+      this.drawElement(this.model.skin.leg, skinLegOffsets);
+      this.drawElement(this.model.skin.leg, skinLegOffsets, true);
+      this.drawElement(this.model.skin.hand, skinHandOffsets);
+      this.drawElement(this.model.skin.hand, skinHandOffsets, true);
+      this.drawElement(
+        this.model.shirtsArm,
+        { x: shirtsArmOffsets.x[this.model.shirtsArm.length], y: shirtsArmOffsets.y },
+        true
+      );
+      this.drawElement(this.model.shirtsArm, {
+        x: shirtsArmOffsets.x[this.model.shirtsArm.length],
+        y: shirtsArmOffsets.y
+      });
+      this.drawElement(this.model.skin.neck, neckOffsets);
+      this.drawElement(this.model.skin.head, headOffsets);
+      this.drawElement(this.model.face, faceOffsets);
+      this.drawElement(this.model.hair, hairOffsets);
+      this.drawElement(this.model.shirt, shirtOffsets);
+      this.drawElement(this.model.shoes, shoesOffsets);
+      this.drawElement(this.model.shoes, shoesOffsets, true);
+      this.drawElement(this.model.pantsLeg, pantsLegOffsets);
+      this.drawElement(this.model.pantsLeg, pantsLegOffsets, true);
+      this.drawElement(this.model.pants, pantsOffsets);
+    },
+    drawElement(element, offsets, mirror = false) {
       let img = new Image();
       img.onload = () => {
         this.ctx.save();
@@ -95,36 +88,23 @@ export default {
           element.y,
           element.width,
           element.height,
-          this.verticalAxis + offsets.x * scale - element.width * scale / 2,
-          this.bottomLine + offsets.y * scale,
-          element.width * scale,
-          element.height * scale
+          this.verticalAxis + offsets.x * this.scale - element.width * this.scale / 2,
+          this.bottomLine + offsets.y * this.scale,
+          element.width * this.scale,
+          element.height * this.scale
         );
         this.ctx.restore();
       };
       img.src = require('../assets/images/models/' + element.file);
-    },
-    drawMob() {
-      this.model.parts.forEach(part => {
-        const img = this.model.images[part];
-        const options = this.model.options[part];
-        this.drawElement(img, options);
-      });
-    },
-
-    // drawHead(part) {
-    //   this.drawElement(part, headOffsets);
-    // },
-
-    // drawShirt(part) {
-    //   this.drawElement(part, shirtOffsets);
-    // },
-    // drawHair(part) {
-    //   this.drawElement(part, hairOffsets);
-    // },
-    // drawNeck(part) {
-    //   this.drawElement(part, neckOffsets);
-    // }
+    }
+  },
+  watch: {
+    renderTrigger: function() {
+      this.draw();
+    }
+  },
+  mounted: function() {
+    this.draw();
   }
 };
 </script>
