2244a6fd40ea5c73b3eda54fa7132706655c8dae
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-02 17:06:24 +0300

    Add stories for the Modal.vue component

--- game-refactoring/src/stories/index.js
+++ game-refactoring/src/stories/index.js
@@ -1,6 +1,12 @@
 import { storiesOf } from '@storybook/vue';
 
 import LoadingView from '../screens/LoadingView.vue';
+import Modal from '../components/Modal.vue';
+import ModalWrapper from './ModalWrapper.vue';
+
+import { LOREM } from './constants.js';
+
+// LoadingView.vue
 
 storiesOf('LoadingView', module)
   .add('loading with a message', () => ({
@@ -11,3 +17,27 @@ storiesOf('LoadingView', module)
     components: { LoadingView },
     template: '<loading-view></loading-view>'
   }));
+
+// Modal.vue
+
+storiesOf('Modal', module)
+  .add('modal with some text', () => ({
+    components: { Modal },
+    data: function() {
+      return { lorem: LOREM };
+    },
+    template: '<modal :show=true>{{ lorem }}</modal>'
+  }))
+  .add('modal with a custom title message', () => ({
+    components: { Modal },
+    template: '<modal :show=true title="Custom header text"></modal>'
+  }))
+  .add('modal with the ok and cancel buttons', () => ({
+    components: { Modal },
+    template: '<modal :show=true hascancel=true></modal>'
+  }));
+
+storiesOf('ModalWrapper', module).add('modal functionality', () => ({
+  components: { ModalWrapper },
+  template: '<modal-wrapper></modal-wrapper>'
+}));
