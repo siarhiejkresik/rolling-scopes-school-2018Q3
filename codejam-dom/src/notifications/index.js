177a458e1203861e81d3261d91b2d15a315652ff
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-12 12:33:24 +0300

    feat: add methods for creating messages and navigation indicators

--- codejam-dom/src/notifications/index.js
+++ codejam-dom/src/notifications/index.js
@@ -1,3 +1,6 @@
+const NAVBAR_INDICATORS_GROUP_NAME = 'indicators';
+const INDICATOR_ID_PATTERN = 'id-';
+const MESSAGE_ELEMENT_CLASS = 'message';
 const KEYS_MAP = {
   PREV: 'Digit1',
   NEXT: 'Digit2',
@@ -24,6 +27,37 @@ export default class Notifier {
       { target: this.root, event: 'click', fn: this.handleNavBarClickEvent },
       { target: window, event: 'keypress', fn: this.handleKeyPressEvent },
     ];
+  }
+  renderMessages(messages) {
+    const fragment = document.createDocumentFragment();
+    messages.forEach((message) => {
+      const li = document.createElement('li');
+      li.classList.add(`${MESSAGE_ELEMENT_CLASS}`);
+      li.innerHTML = message;
+      li.hidden = true;
+      fragment.appendChild(li);
+    });
+    this.messagesArea.appendChild(fragment);
+  }
+
+  renderNavbarIndicators() {
+    const fragment = document.createDocumentFragment();
+    Array(this.numberOfMessages)
+      .fill()
+      .forEach((_, i) => {
+        const id = `${INDICATOR_ID_PATTERN}${i}`;
+        // create indicator
+        const input = document.createElement('input');
+        input.setAttribute('type', 'radio');
+        input.setAttribute('name', NAVBAR_INDICATORS_GROUP_NAME);
+        input.setAttribute('id', id);
+        fragment.appendChild(input);
+        // create label for indicator
+        const label = document.createElement('label');
+        label.setAttribute('for', id);
+        fragment.appendChild(label);
+      });
+    this.navBarIndicators.appendChild(fragment);
   }
   handleKeyPressEvent({ code }) {
     switch (code) {
