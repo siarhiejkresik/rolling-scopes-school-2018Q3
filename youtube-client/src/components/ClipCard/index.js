ead29e5835c3785ab0fdc867379f40471167c985
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-21 00:57:14 +0300

    feat: implement clip card component

--- youtube-client/src/components/ClipCard/index.js
+++ youtube-client/src/components/ClipCard/index.js
@@ -1,10 +1,26 @@
 import './style.css';
 
-export default class {
-  constructor() {
-    this.node = document.createElement('div');
-    this.node.classList.add('card');
-    this.node.required = true;
-    // this.node.innerHTML = '1';
-  }
-}
+export default (data) => {
+  const node = document.createElement('article');
+  node.classList.add('card');
+  const { id, statistics, snippet } = data;
+  const publishedAt = new Date(snippet.publishedAt).toDateString();
+  const viewCount = Number(statistics.viewCount).toLocaleString();
+  node.innerHTML = `
+    <header class="card-header" style="background-image: url('${snippet.thumbnails.medium.url}')">
+      <a href="https://www.youtube.com/watch?v=${id}" target="_blank">
+        <h2>${snippet.title}</h2>
+      </a>
+    </header>
+    <section class="card-body">
+      <div class="card-meta-info">
+        <div class="card-channel"><span>Author:</span> ${snippet.channelTitle}</div>
+        <div class="card-published"><span>Published:</span> ${publishedAt}</div>
+        <div class="card-viewed"><span>Views:</span> ${viewCount}</div>
+      </div>
+      <p class="card-description">${snippet.description}</p> 
+    </section>
+`;
+
+  return node;
+};
