afea29c5e6384a2b27df1a22528f824973db13b6
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-25 01:13:09 +0300

    refactor: einty poin script

--- youtube-client/src/index.js
+++ youtube-client/src/index.js
@@ -3,6 +3,7 @@ import './assets/styles/style.css';
 import SearchBar from './components/SearchBar';
 import ClipsContainer from './components/ClipsContainer';
 import Pagination from './components/Pagination';
+import SwipeableContainer from './components/SwipeableContainer';
 
 // for testing
 import data from '../test/response.json';
@@ -28,16 +29,25 @@ const createPagination = () => {
 const app = createApp();
 const bottom = createBottom();
 const pagination = createPagination();
-const clipsContainer = new ClipsContainer(bottom, pagination);
+const clipsContainer = new ClipsContainer();
 const searchBar = new SearchBar();
+
+// connect clips container with pagination
+clipsContainer.model.pageIndexObserver.subscribe(pagination.goToPage.bind(pagination));
+pagination.pageSelectObserver.subscribe(clipsContainer.onGoToPage.bind(clipsContainer));
+
+// connect clips container with search bar
 searchBar.inputEventObserver.subscribe(clipsContainer.onSearch.bind(clipsContainer));
+
+// make bottom container swipeable
+const swipeable = new SwipeableContainer(bottom, clipsContainer.onSwipe.bind(clipsContainer));
 
 // mount application parts to the DOM
 document.body.appendChild(app);
 app.appendChild(searchBar.node);
 app.appendChild(bottom);
-bottom.appendChild(clipsContainer.node);
+bottom.appendChild(clipsContainer.view.node);
 app.appendChild(pagination.view.node);
 
 // for testing
-clipsContainer.addCards(data);
+// clipsContainer.addCards(data);
