fbebbef070ba1029971957f7dbf6902e34575c12
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-11-25 11:32:23 +0300

    refactor: search bar

---
+++ youtube-client/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,28 @@
+import { DELAY } from './constants';
+
+import Observer from '../../scripts/Observer';
+
+export default class {
+  constructor() {
+    this.node = document.createElement('input');
+    this.node.id = 'search';
+    this.node.type = 'search';
+    this.node.placeholder = 'Search the youtube...';
+    this.node.required = true;
+    this.node.autofocus = true;
+    this.node.autocomplete = 'off';
+    this.node.oninput = this.onInput.bind(this);
+    this.timer = null;
+
+    this.inputEventObserver = new Observer();
+  }
+
+  onInput() {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(this.triggerInputEvent.bind(this), DELAY);
+  }
+
+  triggerInputEvent() {
+    this.inputEventObserver.notify(this.node.value);
+  }
+}
