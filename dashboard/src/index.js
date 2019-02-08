c4c6573b2a83afb981a464d309579d0ccccdb6d6
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-02-08 18:33:00 +0300

    feat: add entry point index.js

---
+++ dashboard/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.css';
+
+import Dashboard from './components/dashboard';
+
+ReactDOM.render(<Dashboard />, document.getElementById('dashboard'));
