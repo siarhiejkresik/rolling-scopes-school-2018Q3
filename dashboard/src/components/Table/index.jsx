b31b8d468a80f3843cdeacbc6b1697e5b0146d30
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-02-22 23:30:33 +0300

    feat: add styles to a dashboard table

--- dashboard/src/components/Table/index.jsx
+++ dashboard/src/components/Table/index.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Head from './Head';
 import Body from './Body';
 
+import './index.css';
+
 export default ({ scores, students, tasks }) => (
-  <table>
-    <TableHead students={students} />
-    <TableBody tasks={tasks} students={students} scores={scores} />
+  <table className="dashboard-table">
+    <Head students={students} />
+    <Body tasks={tasks} students={students} scores={scores} />
   </table>
 );
